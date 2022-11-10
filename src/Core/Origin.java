25a846403e62553a920f32a9a45ac05e11dceab1
MedetHasanUgurlu <mdt.hsn.76@hotmail.com>
2022-11-10 08:14:06 +0300

    .java --> javac compiler --> ByteCode(.class) -->jvm

---
+++ src/Core/Origin.java
@@ -0,0 +1,36 @@
+package Core;
+
+public class Origin {
+
+    // 1995 Sun Microsystem
+    // 2009 Oracle buy it
+
+    // .java --> javac compiler --> ByteCode(.class) -->jvm
+    // Compiler byte koda çevirir.
+    // Byte kodlar jvm tarafından okunur.
+    // JIT = JUST IN TİME sık çalışan kodlar native koda dönüşür, direk işletim sisteminde çalıştırılır.
+    // JNI = JAVA NATIVE INTERFACE ile C kodları çalıştırılabilir.
+
+    // jdk > jre(library +jvm) > jvm(byte kodları makina kodlarına)
+    // garbage collector jvm
+    // jdk = compiler + debugger + javadoc
+
+ // Primitive Data Type
+ //      -boolean
+ //      ->Numeric Type
+ //              ->Integral Type
+ //                      -byte(1byte-128,128)8bit,
+ //                      -short(2byte)16bit,
+ //                      -int(4byte)32bit,
+ //                      -long(8byte)64 bit,
+ //                      -char
+ //              ->Floating Type
+ //                      -float(4byte),
+ //                      -double(8byte)
+
+
+
+
+
+
+ }
